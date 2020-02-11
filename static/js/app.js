ea86080c322194ef19cfe6dad183e890879e620b
Cain Smith <xlqgik@gmail.com>
2020-02-10 23:07:06 -0500

    still need to add home animations, fixed rbridges image and also added customizable backgrounds in the {% block bg %}{% endblock %}

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,16 @@ new fullpage('#fullpage', {
 	//options here
 	autoScrolling: true,
 	navigation: true,
-	scrollHorizontally: false
+
+	/*onLeave: (origin, destination, direction) => {
+		const section = destination.item;
+		const tl = new TimelineMax({ delay: 0.5 });
+		
+		if (destination.index === 1){
+			const path = document.querySelector('.path');
+			const ways = document.querySelector('.way');
+			
+			tl.fromTo(path, 0.7, { x: "100%" }, {x: "-35%"});
+		}
+	}*/
 });
